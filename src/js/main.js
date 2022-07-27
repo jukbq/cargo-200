ebe4604d48dd255570840e209e59304a3a48cf4f
Yurii Ohlii <61588029+jukbq@users.noreply.github.com>
2022-07-27 12:38:30 +0300

    27072022

--- src/js/main.js
+++ src/js/main.js
@@ -2,17 +2,17 @@ $(document).ready(function() {
     "use strict"
 
     let lossesArr = [
-        { img: 'img/icon/1.png', name: 'Танки ', number: 1730 },
-        { img: 'img/icon/2.png', name: 'Бронемашини', number: 3950 },
-        { img: 'img/icon/3.png', name: 'Артелерійські системи', number: 876 },
-        { img: 'img/icon/4.png', name: 'Системи залпового вогню', number: 257 },
-        { img: 'img/icon/5.png', name: 'Засоби ППО', number: 116 },
+        { img: 'img/icon/1.png', name: 'Танки ', number: 1738 },
+        { img: 'img/icon/2.png', name: 'Бронемашини', number: 3971 },
+        { img: 'img/icon/3.png', name: 'Артелерійські системи', number: 883 },
+        { img: 'img/icon/4.png', name: 'Системи залпового вогню', number: 258 },
+        { img: 'img/icon/5.png', name: 'Засоби ППО', number: 117 },
         { img: 'img/icon/6.png', name: 'Літаки', number: 222 },
-        { img: 'img/icon/7.png', name: 'Гелікоптери', number: 188 },
-        { img: 'img/icon/8.png', name: 'Автомобільна техніка і цистерни', number: 2832 },
+        { img: 'img/icon/7.png', name: 'Гелікоптери', number: 190 },
+        { img: 'img/icon/8.png', name: 'Автомобільна техніка і цистерни', number: 2847 },
         { img: 'img/icon/9.png', name: 'Кораблі (катери)', number: 15 },
         { img: 'img/icon/10.png', name: 'Крилаті ракети', number: 174 },
-        { img: 'img/icon/12.png', name: 'БПЛА', number: 719 },
+        { img: 'img/icon/12.png', name: 'БПЛА', number: 726 },
         { img: 'img/icon/11.png', name: 'Спецтехніка', number: 73 },
     ];
 
