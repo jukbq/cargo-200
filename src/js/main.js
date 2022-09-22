79efcfa2c363c1ec687b68e6a239e6af97dfa670
Yurii Ohlii <61588029+jukbq@users.noreply.github.com>
2022-09-22 16:48:33 +0300

    Підключення API

--- src/js/main.js
+++ src/js/main.js
@@ -1,47 +1,71 @@
 $(document).ready(function() {
     "use strict"
-
-    let lossesArr = [
-        { img: 'img/icon/1.png', name: 'Танки ', number: 2180 },
-        { img: 'img/icon/2.png', name: 'Бронемашини', number: 4665 },
-        { img: 'img/icon/3.png', name: 'Артелерійські системи', number: 1290 },
-        { img: 'img/icon/4.png', name: 'Системи залпового вогню', number: 311 },
-        { img: 'img/icon/5.png', name: 'Засоби ППО', number: 167 },
-        { img: 'img/icon/6.png', name: 'Літаки', number: 246 },
-        { img: 'img/icon/7.png', name: 'Гелікоптери', number: 215 },
-        { img: 'img/icon/12.png', name: 'БПЛА', number: 908 },
-        { img: 'img/icon/10.png', name: 'Крилаті ракети', number: 233 },
-        { img: 'img/icon/9.png', name: 'Кораблі (катери)', number: 15 },
-        { img: 'img/icon/8.png', name: 'Автомобільна техніка і цистерни', number: 3501 },
-        { img: 'img/icon/11.png', name: 'Спецтехніка', number: 120 },
-    ];
+    let run = document.querySelector('section')
 
 
 
-    let losses = ''
-    $.each(lossesArr, function(i, value) {
-        losses += '<div class="block">'
-        losses += '<p class="name">' + value['name'] + '</p>'
-        losses += '<div class="icon">'
-        losses += '<img src="../' + value['img'] + '" alt="">'
-        losses += '</div>'
-        losses += '<div class="title">'
-        losses += '<p><span class="counter">' + value['number'] + '</span></p>'
-        losses += '</div>'
-        losses += '</div>'
+    async function load_rip(e) {
+        console.log('11111');
+        const api = "https://russianwarship.rip/api/v1/statistics/latest"
+        const api_url = await fetch(api, {
+            method: 'GET',
+        })
+        const data = await api_url.json();
+        if (api_url.ok) {
+            get_data(data)
+        } else {
+            console.log('error');
+        }
+    }
 
-    });
 
-    $('.cradh').html(losses)
+    function get_data(data) {
 
-    $('.counter').counterUp({
-        delay: 10,
-        time: 1500
-    });
-    $('.counter').addClass('animated fadeInDownBig');
-    $('h3').addClass('animated fadeIn');
+        let dead = data.data.stats.personnel_units
+        $('.counter').html(dead)
+        let data_arr = [
+            { img: 'img/icon/1.png', name: 'Танки ', number: data.data.stats.tanks },
+            { img: 'img/icon/3.png', name: 'Артелерійські системи', number: data.data.stats.artillery_systems },
+            { img: 'img/icon/4.png', name: 'Системи залпового вогню', number: data.data.stats.mlrs },
+            { img: 'img/icon/5.png', name: 'Засоби ППО', number: data.data.stats.aa_warfare_systems },
+            { img: 'img/icon/6.png', name: 'Літаки', number: data.data.stats.planes },
+            { img: 'img/icon/7.png', name: 'Гелікоптери', number: data.data.stats.helicopters },
+            { img: 'img/icon/12.png', name: 'БПЛА', number: data.data.stats.uav_systems },
+            { img: 'img/icon/10.png', name: 'Крилаті ракети', number: data.data.stats.cruise_missiles },
+            { img: 'img/icon/9.png', name: 'Кораблі (катери)', number: data.data.stats.warships_cutters },
+            { img: 'img/icon/8.png', name: 'Автомобільна техніка і цистерни', number: data.data.stats.vehicles_fuel_tanks },
+            { img: 'img/icon/11.png', name: 'Спецтехніка', number: data.data.stats.special_military_equip },
+        ];
 
-    let url = 'https://zaxid.net/vtrati_rosiyan_u_viyni_proti_ukrayini_n1537635'
+        let losses = ''
+        $.each(data_arr, function(i, value) {
+            losses += '<div class="block">'
+            losses += '<p class="name">' + value['name'] + '</p>'
+            losses += '<div class="icon">'
+            losses += '<img src="../' + value['img'] + '" alt="">'
+            losses += '</div>'
+            losses += '<div class="title">'
+            losses += '<p><span class="counter">' + value['number'] + '</span></p>'
+            losses += '</div>'
+            losses += '</div>'
+        });
+        $('.cradh').html(losses)
+
+        $('.counter').counterUp({
+            delay: 10,
+            time: 1500
+        });
+        $('.counter').addClass('animated fadeInDownBig');
+        $('h3').addClass('animated fadeIn');
+
+    }
+
+    if (run) {
+        load_rip()
+    }
+
+
+
 
 
 
